61e099cfeea7ccff6a1820003f30ba941891224a
anikeenko <dimskay1988@icloud.com>
2024-02-07 11:59:27 +0300

    ADDED the ability to change the device

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from service import get_temperature
 
+import json
+
 app = FastAPI()
+
+
+class UpdateDataDevice(BaseModel):
+    sensor_address: str
 
 
 @app.get("/")
@@ -12,5 +19,33 @@ async def read_root():
 
 @app.get("/get_temperature")
 async def temperature():
-    temp = get_temperature()
+    url = read_data().get("sensor_address")
+    temp = get_temperature(url)
     return {"temperature": temp}
+
+
+json_file_path = "data.json"
+
+
+def read_data():
+    with open(json_file_path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def write_data(data):
+    with open(json_file_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+@app.get("/get_device", description="An example of what the device address should be http://192.168.1.151")
+async def read_data_endpoint():
+    return read_data()
+
+
+@app.put("/update_device", description="An example of what the device address should be http://192.168.1.151")
+async def update_data_endpoint(request_data: UpdateDataDevice):
+    current_data = read_data()
+    current_data.update(request_data)
+    write_data(current_data)
+    return {"message": "Data updated successfully"}
--- service.py
+++ service.py
@@ -1,26 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from fastapi import HTTPException
 
 
-def get_temperature():
-    url = 'http://192.168.1.151/status.xml'
+def get_temperature(url):
+    link = f'{url}/status.xml'
     try:
-        response = requests.get(url, timeout=3)
+        response = requests.get(link, timeout=3)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
-
             temperature_element = soup.find('s33301')
 
             if temperature_element:
                 temperature = float(temperature_element.text.strip().split(' ')[0])
                 return temperature
             else:
-                raise ValueError("Temperature element not found")
+                raise HTTPException(status_code=400, detail="Temperature element not found")
         else:
-            raise Exception("Error executing the request:", response.status_code)
+            raise HTTPException(status_code=400, detail=f"Error executing the request: {response.status_code}")
     except Exception as e:
-        raise e
-
-
-
+        raise HTTPException(status_code=400, detail=f"Error executing the request: {str(e)}")
